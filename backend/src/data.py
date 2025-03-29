785f11fca75e1a63b2ede726f437a5fa8b385ac7
tayydev <51377975+tayydev@users.noreply.github.com>
2025-03-29 19:27:40 -0400

    user public

--- backend/src/data.py
+++ backend/src/data.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -7,13 +8,33 @@ class QuestionStatus(BaseModel):
     estimated_seconds: int
 
 class Message(BaseModel):
+    user_id: str
     name: str
     content: str
     is_answer: bool
 
 class Session(BaseModel):
+    user_id: str  # uuid
     title: str
     created_at: str = datetime.now().strftime("%Y-%m-%d")
     created_timestamp: datetime = datetime.now()
     uuid: str
     content: list[Message] = []
+
+class UserPublic(BaseModel):
+    uuid: str
+    display_name: str
+    pfp_url: str
+
+class UserSecret(BaseModel):
+    uuid: str
+    display_name: str
+    pfp_url: str
+    # todo: google secret
+
+    def to_pub(self):
+        return UserPublic(
+            uuid=self.uuid,
+            display_name=self.display_name,
+            pfp_url=self.pfp_url
+        )
--- backend/src/service.py
+++ backend/src/service.py
@@ -1,25 +1,36 @@
+import random
 from typing import Optional
 from uuid import uuid4
 
 from data import *
 
-storage: dict[str, Session] = {}
+session_store: dict[str, Session] = {}
+user_store: dict[str, UserSecret] = {}
 
 def get_only(uuid: str):
-    return storage[uuid]
+    return session_store[uuid]
 
 def get_sessions():
     sessions = []
-    for session in storage.values():
+    for session in session_store.values():
         sessions.append(session)
     return sessions
 
 def get_or_create(uuid: Optional[str], title: str):
     if uuid:
-        return storage[uuid]
+        return session_store[uuid]
     else:
         return Session(uuid=str(uuid4()), title=title)
 
 def write_session(session: Session):
-    storage[session.uuid] = session
+    session_store[session.uuid] = session
     return session
+
+def create_temp_user() -> UserPublic:
+    guest_id = str(uuid4())
+    user_store[guest_id] = UserSecret(
+        uuid=guest_id,
+        display_name = "guest_user_" + str(random.randint(10000000, 99999999)),
+        pfp_url="todo",
+    )
+    return user_store[guest_id].to_pub()
