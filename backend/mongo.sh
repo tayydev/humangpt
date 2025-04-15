88bd813a151759eecf18cad36be3ea6f43b259ec
tayydev <51377975+tayydev@users.noreply.github.com>
2025-04-15 16:08:40 -0400

    restart mongo

--- backend/mongo.sh
+++ backend/mongo.sh
@@ -6,6 +6,7 @@ docker run -d \
   -e MONGO_INITDB_ROOT_USERNAME=username \
   -e MONGO_INITDB_ROOT_PASSWORD=password123 \
   --mount type=tmpfs,destination=/data/db \
+  --restart=always \
   mongo:latest
 
 echo "MongoDB container started on port 27017"
